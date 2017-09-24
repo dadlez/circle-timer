5fc62e328605f98126feda4ca21db8480f73855e
dadlez <lukaszdadlez@gmail.com>
2017-09-25 00:19:18 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+$( document ).ready(function(event) {
+	console.log("ready");
+
+	const startMin = 0;
+	const startSec = 10;
+	let min = startMin;
+	let sec = startSec;
+	console.log(min, sec);
+	$('.min').text(min)
+	$('.sec').text(sec)
+
+
+	const countdown = (min, sec) => {
+		if (sec === 0 && min > 0) {
+			min -= 1;
+			sec = 59;
+		} else if (sec === 0 && min === 0) {
+			stopTimer();
+		} else {
+			sec -= 1;
+		}
+
+		return [min, sec]
+	}
+
+
+	const timer = setInterval(function() {
+		let x = 360 * ((min * 60 + sec) / (startMin * 60 + startSec));
+		console.log(x);
+		[ min, sec ] = countdown(min, sec)
+
+		$('.box__circle').css('transform', `rotateZ(${x}deg)`)
+		$('.min').text(min)
+		$('.sec').text(sec)
+	}, 1000);
+
+	const stopTimer = function() { clearInterval(timer) }
+});
